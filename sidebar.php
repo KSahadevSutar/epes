48b1c0423394f8e028d024cf5f56153e83eae4cd
Kiran Sutar <kiran.sahadevsutar@equinox.com>
2021-12-25 00:09:54 +0530

    MT changes

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <div class="dropdown">
-   	<a href="./" class="brand-link">
+    <a href="./" class="brand-link" style="background-color: white;">
         <?php if($_SESSION['login_type'] == 2): ?>
-        <h3 class="text-center p-0 m-0"><b>ADMIN</b></h3>
+          <img src="./assets//Mouritech-R-Logo.png" alt="MOURI Tech" class="dark-logo" style="width: 200px;padding-left: 20px;">
         <?php elseif($_SESSION['login_type'] == 1): ?>
         <h3 class="text-center p-0 m-0"><b>Evaluator</b></h3>
          <?php else: ?>
